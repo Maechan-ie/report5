49f38a79d8926375aacf5516832b33dcf5ca5b57
Yuugo Maeshiro <e215735@ie.u-ryukyu.ac.jp>
2021-12-20 16:28:36 +0900

    step2

--- Report5.java
+++ Report5.java
@@ -1,7 +1,15 @@
 class Report5{
     public static void main(String[] args){
-    String str = null;
-    System.out.println(str.length());
+        String str = null;
+        try{
+            System.out.println(str.length());
+        }catch(NullPointerException e){
+            System.out.println("NullPointerException発生");
+            System.out.println(e.getMessage());
+
+
+        }
+    
     }
      
-    }
+}
